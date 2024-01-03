70c67b18083209f3d2a3e6cfbae4554b85d1e40b
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-01-03 14:03:23 +0500

    setting editing

--- Sayt/settings.py
+++ Sayt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_y8ou!aqx+s!*%s0sb*cyn5kdos%i$$$=e+6geakb=2jo^^y@x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
